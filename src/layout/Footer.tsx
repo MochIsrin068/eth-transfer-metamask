18f5bbad2dccd771b51809fabac8faea24a2cd8a
MochIsrin068 <isrin068@gmail.com>
2023-05-27 13:43:35 +0800

    feat: adjust header & footer layout

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         alt="wave-bottom"
         className="absolute bottom-12 z-0 opacity-10"
       />
-      <div className="bg-white w-full py-4 px-14 flex items-center justify-center z-10 relative">
+      <div className="bg-white w-full py-3 lg:py-4 px-4 lg:px-14 flex items-center justify-center z-10 relative text-sm lg:text-base">
         <p className="font-bold">
           &copy; 2023 -{" "}
           <a
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -6,10 +6,10 @@ import Button from "../components/Button";
 export default function Header() {
   return (
     <section>
-      <menu className="flex items-center justify-between bg-white w-full py-4 px-14 z-10 relative">
+      <menu className="flex items-center justify-between bg-white w-full py-3 lg:py-4 px-4 lg:px-14 z-10 relative">
         <div className="flex items-center">
-          <img src={EthLogo} alt="ETH" className="w-9 mr-2" />
-          <h3 className="font-extrabold text-xl">ETH Transfer</h3>
+          <img src={EthLogo} alt="ETH" className="w-7 lg:w-9 mr-2" />
+          <h3 className="font-extrabold text-base lg:text-xl">ETH Transfer</h3>
         </div>
         {/* <Button label="Connect E-wallet" /> */}
         <img
